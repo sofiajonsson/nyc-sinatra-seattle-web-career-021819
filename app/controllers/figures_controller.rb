dedf208e8679e7c8cc47cf3009e8df6ee6835e46
isalevine <isaacdlevine@gmail.com>
2019-03-15 09:21:29 -0700

    labwork from 3/14/19--some tests incomplete

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,46 @@
 class FiguresController < ApplicationController
   # add controller methods
+  set :views, 'app/views/figures'
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :index
+  end
+
+  get '/figures/new' do
+    @titles=Title.all
+    @landmarks=Landmark.all
+    erb :new
+  end
+
+  post '/figures' do
+    # binding.pry
+    new_figure = Figure.create(params[:figure])
+    # binding.pry
+      # puts "The current Figure.all.count is " + Figure.all.count.to_s
+    FigureTitle.create(title_id: params[:figure][:title_ids][0] , figure_id: new_figure.id)
+
+# binding.pry
+    redirect to '/figures'
+    # "/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :show
+  end
+
+#update
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :edit
+  end
+
+  patch '/figures/:id' do
+    figure = Figure.find(params[:id])
+    # binding.pry
+    figure.update(params[:figures])
+    redirect "/figures/#{figure.id}"
+  end
+
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,34 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+set :views, 'app/views/landmarks'
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :index
+  end
+
+  get '/landmarks/new' do
+    erb :new
+  end
+
+  post '/landmarks' do
+    Landmark.create(params[:landmark])
+    redirect to '/landmarks'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :show
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :edit
+  end
+
+  patch '/landmarks/:id' do
+    landmark = Landmark.find(params[:id])
+    landmark.update(params[:landmarks])
+    redirect "/landmarks/#{landmark.id}"
+  end
+
 end
